2d759a59ab9bfa32c35e55469688df24916d3430
TH-Raju <rajukhan894200@gmail.com>
2022-09-26 15:06:08 +0600

    fakeDB added

--- src/componants/Shop/Shop.js
+++ src/componants/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -17,6 +18,7 @@ const Shop = () => {
     const handleAddToClick = (product) => {
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id);
     }
 
     return (
