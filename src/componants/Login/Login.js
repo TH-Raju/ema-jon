62035c59b66dbfbee018200953924a6aac29b3dd
TH-Raju <rajukhan894200@gmail.com>
2022-10-17 10:09:32 +0600

    Log in page Designe

--- src/componants/Login/Login.js
+++ src/componants/Login/Login.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css';
 
 const Login = () => {
     return (
-        <div>
-            Log in
+        <div className='form-container'>
+            <h2 className="form-title">Log in</h2>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name='email' id='' required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name='password' id='' required />
+                </div>
+                <input type="submit" className='btn-submit' value="Login" />
+            </form>
+            <p>New to Ema-Jon <Link to='/signup'>Create a new account</Link> </p>
+
         </div>
     );
 };
